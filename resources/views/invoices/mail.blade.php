4c911e30226d3846ee686a5ef35a6e03bd8cd41c
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-02-06 11:19:23 +0100

    Ajouter le style CSS pour le mail de facture

--- resources/views/invoices/mail.blade.php
+++ resources/views/invoices/mail.blade.php
@@ -1,3 +1,33 @@
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+    }
+    .container {
+        max-width: 600px;
+        margin: 20px auto;
+        padding: 20px;
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    h1 {
+        color: #333;
+    }
+    p {
+        color: #666;
+    }
+</style>
+</head>
 <body>
-    <h1>Salut c'est un mail test</h1>
+<div class="container">
+    <h1>Votre devis en pièce jointe</h1>
+    <p>Bonjour,</p>
+    <p>Voici votre devis que vous retrouverez en pièce jointe</p>
+    <p>N'hésitez pas à nous contacter si vous avez des questions ou des préoccupations.</p>
+    <p>Merci de votre confiance.</p>
+    <br>
+</div>
 </body>
