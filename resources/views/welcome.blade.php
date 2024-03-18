1431a8d491fee833fcd690c5a58ed25fe1a0457b
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-03-18 12:45:57 +0100

    Mise à jour de la page d'accueil avec des modifications de mise en page et de contenu.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,20 +13,73 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body>
-        <div class=" bg-[#f3f4f6] selection:bg-[#E63462] selection:text-white">
+    <body class="bg-[#f3f4f6] selection:bg-[#E63462] selection:text-white">
+        <div class="  mx-auto w-[90%] ">
             @if (Route::has('login'))
                 <livewire:welcome.navigation />
             @endif
 
-            <header class="min-h-screen flex justify-center items-center mb-16">
-                <img src="{{ asset("img/robot.png")}}" class="h-[45%] w-[45%]">
+            <header class="min-h-screen flex justify-between items-center">
+                <img src="{{ asset("img/robot.png")}}" class="h-[30%] w-[30%]">
                 <div>
                 <h1 class=" text-8xl font-bold">Rencontres de Robotique GEII</h1>
-                <button class="bg-[#E63462] hover:bg-red-600  font-bold py-4 px-8 text-2xl rounded mt-4">Plus d'informations</button>
-                <div>
+                <p class=" text-2xl">Bienvenue sur le site des rencontres de robotique GEII</p>
+                </div>
             </header>
 
+            <main class="flex flex-col items-center justify-center mb-16">
+
+
+                <ol class="relative border-s border-gray-200 dark:border-gray-700 w-1/2 mb-16">
+                    <li class="mb-10 ms-4">
+                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Inscription sur le site</h3>
+                        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Création du compte utilisateur puis attente de sa validation.</p>
+                    </li>
+                    <li class="mb-10 ms-4">
+                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Confirmation d'inscription sur le site</h3>
+                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Une fois la validation reçue dans votre boîte mail, vous pouvez à nouveau vous connecter sur le site et voir les différentes compétitions de cette année ainsi que les challenges. Vous pouvez aussi accéder à la partie forum pour pouvoir poser des questions ou bien communiquer sur les différentes compétitions.</p>
+                    </li>
+                    <li class="ms-4">
+                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Envie de s'inscrire ?</h3>
+                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Une fois que vous avez choisi la compétition où vous voulez vous inscrire, rien de plus simple : vous devez tout d'abord rentrer votre adresse de facturation, puis ensuite vous rendre dans la nouvelle partie disponible, à savoir "Équipes". Dans cette partie, vous allez pouvoir directement créer et gérer vos équipes afin de pouvoir les inscrire à une compétition. Une fois votre équipe créée et inscrite au tournoi, il ne vous reste plus qu'à vérifier si le devis généré est bien le bon, puis à appuyer sur le bouton "recevoir par mail" pour l'obtenir dans votre boîte mail, et l'envoyer aux organisateurs de la compétition.</p>
+                    </li>
+                </ol>
+
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    <div class="relative overflow-hidden bg-gray-200 dark:bg-gray-700 rounded-lg">
+                        <img src="https://placehold.co/600x400" alt="Image 1" class="w-full h-full object-cover">
+                    </div>
+                    <div class="relative overflow-hidden bg-gray-200 dark:bg-gray-700 rounded-lg">
+                        <img src="https://placehold.co/600x400" alt="Image 2" class="w-full h-full object-cover">
+                    </div>
+                    <div class="relative overflow-hidden bg-gray-200 dark:bg-gray-700 rounded-lg">
+                        <img src="https://placehold.co/600x400" alt="Image 3" class="w-full h-full object-cover">
+                    </div>
+                    <div class="relative overflow-hidden bg-gray-200 dark:bg-gray-700 rounded-lg">
+                        <img src="https://placehold.co/600x400" alt="Image 4" class="w-full h-full object-cover">
+                    </div>
+                    <div class="relative overflow-hidden bg-gray-200 dark:bg-gray-700 rounded-lg">
+                        <img src="https://placehold.co/600x400" alt="Image 5" class="w-full h-full object-cover">
+                    </div>
+                    <div class="relative overflow-hidden bg-gray-200 dark:bg-gray-700 rounded-lg">
+                        <img src="https://placehold.co/600x400" alt="Image 6" class="w-full h-full object-cover">
+                    </div>
+
+
+                
+
+            </main>
+
+
+
+
         </div>
+
+        <footer class="p-8 bg-gray-200 w-full">
+            <p class="text-center">© 2024 - Rencontres de Robotique GEII - Créer par <a href="https://eloick.fr" target="blank" class="underline text-red-500">Eloïck </a></p>
+        </footer>
     </body>
 </html>
