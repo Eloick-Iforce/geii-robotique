6439403f13a898e8f6f6d886330f2baae9ecf9e6
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-03-18 12:49:42 +0100

    Ajouter des fonctionnalités de routage et de schéma dans AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+
+        $base_path = parse_url(url('/'), PHP_URL_PATH);
+
+        if ($base_path) {
+            Livewire::setScriptRoute(function ($handle) use ($base_path) {
+                return Route::get($base_path . '/livewire/livewire.js', $handle);
+            });
+            Livewire::setUpdateRoute(function ($handle) use ($base_path) {
+                return Route::post($base_path . '/livewire/update', $handle);
+            });
+        }
     }
 }
