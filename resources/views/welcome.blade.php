d047498dad11fde2caf20874247840817313d4ae
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-03-23 17:10:18 +0100

    Mise à jour de la vue welcome.blade.php pour afficher une timeline des étapes d'inscription sur le site.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,23 +30,52 @@
             <main class="flex flex-col items-center justify-center mb-16">
 
 
-                <ol class="relative border-s border-gray-200 dark:border-gray-700 w-1/2 mb-16">
-                    <li class="mb-10 ms-4">
-                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Inscription sur le site</h3>
-                        <p class="mb-4 text-base font-normal text-justify text-gray-500 dark:text-gray-400">Création du compte utilisateur puis attente de sa validation.</p>
+                <ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical w-1/2 text-justify">
+                    <li>
+                      <div class="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+                      </div>
+                      <div class="timeline-start md:text-end mb-10">
+                        <time class="font-mono italic">Mars</time>
+                        <div class="text-lg font-black">Inscription sur le site</div>
+                        <p class="w-full text-justify">Création du compte utilisateur puis attente de sa validation.</p>
+                      </div>
+                      <hr/>
                     </li>
-                    <li class="mb-10 ms-4">
-                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Confirmation d'inscription sur le site</h3>
-                        <p class="text-base font-normal text-gray-500 text-justify dark:text-gray-400">Une fois la validation reçue dans votre boîte mail, vous pouvez à nouveau vous connecter sur le site et voir les différentes compétitions de cette année ainsi que les challenges. Vous pouvez aussi accéder à la partie forum pour pouvoir poser des questions ou bien communiquer sur les différentes compétitions.</p>
+                    <li>
+                      <hr />
+                      <div class="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+                      </div>
+                      <div class="timeline-end mb-10">
+                        <div class="text-lg font-black ">Confirmation d'inscription sur le site</div>
+                        <p class="w-full text-justify">Une fois la validation reçue dans votre boîte mail, vous pouvez à nouveau vous connecter sur le site et voir les différentes compétitions de cette année ainsi que les challenges. Vous pouvez aussi accéder à la partie forum pour pouvoir poser des questions ou bien communiquer sur les différentes compétitions.</p>
+                      </div>
+                      <hr />
                     </li>
-                    <li class="ms-4">
-                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Envie de s'inscrire ?</h3>
-                        <p class="text-base font-normal text-gray-500 text-justify dark:text-gray-400">Une fois que vous avez choisi la compétition où vous voulez vous inscrire, rien de plus simple : vous devez tout d'abord rentrer votre adresse de facturation, puis ensuite vous rendre dans la nouvelle partie disponible, à savoir "Équipes". Dans cette partie, vous allez pouvoir directement créer et gérer vos équipes afin de pouvoir les inscrire à une compétition. Une fois votre équipe créée et inscrite au tournoi, il ne vous reste plus qu'à vérifier si le devis généré est bien le bon, puis à appuyer sur le bouton "recevoir par mail" pour l'obtenir dans votre boîte mail, il sera envoyé automatiquement aux organisateurs.</p>
+                    <li>
+                      <hr />
+                      <div class="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+                      </div>
+                      <div class="timeline-start md:text-end mb-10">
+                        <div class="text-lg font-black ">Envie de s'inscrire ?</div>
+                        <p class="w-full text-justify">Une fois que vous avez choisi la compétition où vous voulez vous inscrire, rien de plus simple : vous devez tout d'abord rentrer votre adresse de facturation, puis ensuite vous rendre dans la nouvelle partie disponible, à savoir "Équipes". Dans cette partie, vous allez pouvoir directement créer et gérer vos équipes afin de pouvoir les inscrire à une compétition. Une fois votre équipe créée et inscrite au tournoi, il ne vous reste plus qu'à vérifier si le devis généré est bien le bon, puis à appuyer sur le bouton "recevoir par mail" pour l'obtenir dans votre boîte mail, il sera envoyé automatiquement aux organisateurs.</p>
+                      </div>
+                      <hr />
                     </li>
-                </ol>
+                    <li>
+                      <hr />
+                      <div class="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+                      </div>
+                      <div class="timeline-end mb-10">
+                        <time class="font-mono italic">Juin</time>
+                        <div class="text-lg font-black">Début de la compétition</div>
+    
+                      </div>
+                    </li>
+                  </ul>
 
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                     <div class="relative overflow-hidden bg-gray-200 dark:bg-gray-700 rounded-lg">
